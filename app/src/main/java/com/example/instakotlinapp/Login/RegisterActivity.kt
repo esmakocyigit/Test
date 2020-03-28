07a98bbee96821eb3e4854d876cea73f6584225d
Esma Koçyiğit <kocyigitesma@outlook.com>
2020-03-29 00:52:57 +0300

    25

---
+++ app/src/main/java/com/example/instakotlinapp/Login/RegisterActivity.kt
@@ -0,0 +1,68 @@
+package com.example.instakotlinapp.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import com.example.instakotlinapp.R
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        init()
+    }
+
+    private fun init() {
+        tvEposta.setOnClickListener {
+            viewTelefon.visibility=View.INVISIBLE
+            viewEposta.visibility=View.VISIBLE
+            etGirisYontemi.setText("")
+            etGirisYontemi.inputType=InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            etGirisYontemi.setHint("E-Posta")
+            btnIleri.isEnabled=false
+        }
+        tvTelefon.setOnClickListener {
+            viewTelefon.visibility=View.VISIBLE
+            viewEposta.visibility=View.INVISIBLE
+            etGirisYontemi.setText("")
+            etGirisYontemi.inputType=InputType.TYPE_CLASS_NUMBER
+            etGirisYontemi.setHint("Telefon")
+            btnIleri.isEnabled=false
+        }
+
+        etGirisYontemi.addTextChangedListener (object :TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (start+before+count >= 10){
+                    btnIleri.isEnabled=true
+                    btnIleri.setTextColor(ContextCompat.getColor(this@RegisterActivity,R.color.beyaz))
+                    btnIleri.setBackgroundResource(R.drawable.register_button_aktif)
+                }
+                else{
+
+                    btnIleri.isEnabled=false
+                    btnIleri.setTextColor(ContextCompat.getColor(this@RegisterActivity,R.color.sonukmavi))
+                    btnIleri.setBackgroundResource(R.drawable.register_button)
+
+
+                }
+            }
+
+
+        })
+    }
+}
